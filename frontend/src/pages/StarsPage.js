1b80c934d1ab8a79aa4e110de2007aece3291a5c
kblanchfield <kateblanchfield@live.co.uk>
2018-11-16 16:17:28 +0100

    Calculate local sidereal time

--- frontend/src/pages/StarsPage.js
+++ frontend/src/pages/StarsPage.js
@@ -1,18 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
+import PropTypes from "prop-types"
 import Constellation from "../components/Constellation"
+import Moment from "react-moment"
+
+// es5 usage
+const moment = require("moment")
 
 class StarsPage extends Component {
 
   state = {
-    longitude: 18.0686,
-    latitude: 59.3293,
-    lstHours: 3,
-    lstMins: 48,
-    visibleStars: []
+    visibleStars: [],
+    collectedStars: []
   }
 
   componentDidMount() {
-    const url = `http://localhost:8080/stars?latitude=${this.state.latitude}&lstHours=${this.state.lstHours}`
+    this.getVisibleConstellations()
+  }
+
+  calculateSideRealTime = () => {
+    // calculations
+    const UT = moment().utc() // needs to be midnight tonight
+    console.log(UT)
+
+    // days since 2000
+    let today = moment()
+    let J2000 = moment("2000-01-01 24:00:00.000")
+    let daysSince2000 = today.diff(J2000, "days", true)
+    console.log("daysSince2000", daysSince2000)
+
+    let greenwichst = (280.46061837 + (360.98564736629 * daysSince2000)) % 360
+    console.log("Greenwich ST", greenwichst)
+
+    let localst = (greenwichst + this.props.lng) % 360
+    console.log("Local ST", localst)
+
+    let lst = (localst / 360) * 24
+    console.log(lst)
+
+    //const lst = 100.46 + (0.985647 * daysSince2000) + this.props.lng + (15 * UT)
+
+    const lstHours = Math.round(lst)
+    console.log(lstHours)
+    return ({
+      lstHours: lstHours
+    })
+  }
+
+  getVisibleConstellations= () => {
+    const time = this.calculateSideRealTime()
+    console.log(time.lstHours)
+    const url = `http://localhost:8080/stars?latitude=${this.props.lat}&lstHours=${time.lstHours}`
     fetch(url)
       .then(response => response.json())
       .then(json => {
@@ -20,26 +57,49 @@ class StarsPage extends Component {
           visibleStars: json
         })
       })
+      .catch(err => console.log("error:", err))
   }
 
   render() {
 
-    return (
-      <div className="page">
-        <div className="left">
-          There are 88 recognised constellations in the night sky.
-          In ---- you'll see ---.
-          And --- you haven't seen before.
+    if (this.state.visibleStars.length > 0) {
+      return (
+        <div className="page">
+          <div className="left">
+            There are 88 recognised constellations in the night sky.
+            In {this.props.lat} you'll see ---- constellations.
+            And --- you haven't seen before.
+          </div>
+          <div className="right">
+            {this.state.visibleStars.map(constellation =>
+               <Constellation
+                constellation={constellation}
+                key={constellation}
+               />
+            )}
+          </div>
         </div>
-        <div className="right">
-          {this.state.visibleStars.map(constellation => {
-            return <Constellation constellation={constellation} />
-          })}
+      )
+    } else {
+      return (
+        <div className="page">
+          <div className="left">
+            There are 88 recognised constellations in the night sky.
+            From the North Pole you'll see ---- constellations.
+          </div>
+          <div className="right">
+            Right
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
 
   }
+}
+
+StarsPage.propTypes = {
+  lat: PropTypes.number,
+  lng: PropTypes.number
 }
 
 export default StarsPage
