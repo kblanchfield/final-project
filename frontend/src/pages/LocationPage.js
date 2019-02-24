1f13510d6eabd3e2af1f62db910404f643ba25fc
kblanchfield <kateblanchfield@live.co.uk>
2019-02-24 19:14:22 +0100

    Move functions out of component + async / await

--- frontend/src/pages/LocationPage.js
+++ frontend/src/pages/LocationPage.js
@@ -4,13 +4,13 @@ import LocationText from "../components/LocationText"
 import BarChart from "../components/BarChart"
 import Map from "../components/Map"
 import { LocationPageWrapper,  LocationLeftWrapper, ChartTitle, LocationRightWrapper } from "../styledComponents/LocationPageStyles"
-
+import { getWeatherData } from "./LocationPageLogic"
 
 class LocationPage extends Component {
 
   state = {
-    lat: 0,
-    lng: 0,
+    lng: 18.063240,
+    lat: 59.334591,
     weatherMessage: "",
     midnightCloudCover: []
   }
@@ -36,47 +36,18 @@ class LocationPage extends Component {
     this.getWeather()
   }
 
-  getWeather = () => {
-    const { lat,  lng } = this.state
-
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&APPID=ee4066bbc82305811aecd61e6c30d861`)
-      .then(response =>
-      response.json())
-      .then(json => {
-        if(json.list !== undefined) {
-          const midnightCloudCover = json.list.filter(time => /00:00:00/.test(time.dt_txt)).map(day => day.clouds.all)
-          this.setState({
-            midnightCloudCover
-          })
-        }
-      })
-      .then(() => {
-        const { midnightCloudCover } = this.state
-
-        let weatherMessage = ""
-        if (midnightCloudCover[0] < 50) {
-          weatherMessage = `Good news! Looks like it will be a clear sky at midnight tonight - perfect for seeing the stars.`
-        } else if (midnightCloudCover[1] < 50) {
-          weatherMessage = `There's a ${midnightCloudCover[0]}% chance of clouds tonight. It looks like it will be a clearer sky tomorrow night.`
-        } else if (midnightCloudCover[2] < 50) {
-          weatherMessage = `There's a ${midnightCloudCover[0]}% chance of clouds tonight. It looks like it will be a clearer sky in 2 nights.`
-        } else if (midnightCloudCover[3] < 50) {
-          weatherMessage = `There's a ${midnightCloudCover[0]}% chance of clouds tonight. It looks like it will be a clearer sky in 3 nights.`
-        } else if (midnightCloudCover[4] < 50) {
-          weatherMessage = `There's a ${midnightCloudCover[0]}% chance of clouds tonight. It looks like it will be a clearer sky in 4 nights.`
-        } else {
-          weatherMessage = `Looks like it wll be cloudy for the next 5 nights. Netflix?`
-        }
-        this.setState({
-          weatherMessage
-        })
-      })
-      .catch(err => console.log("err:", err))
+  getWeather = async () => {
+    const { lat, lng } = this.state
+    const weatherData = await getWeatherData(lat, lng)
+    this.setState({
+      midnightCloudCover: weatherData.midnightCloudCover,
+      weatherMessage: weatherData.weatherMessage
+    })
   }
 
 
   render() {
-    const { lat,  lng, weatherMessage, midnightCloudCover } = this.state
+    const { lat, lng, weatherMessage, midnightCloudCover } = this.state
 
     return (
       <LocationPageWrapper>
---
+++ frontend/src/pages/LocationPageLogic.js
@@ -0,0 +1,29 @@
+
+const getMidnightCloudCover = json => {
+  return json.list.filter(time => /00:00:00/.test(time.dt_txt))
+    .map(day => day.clouds.all)
+  }
+
+const getWeatherMessage = midnightCloudCover => {
+  if (midnightCloudCover[0] < 50) {
+    return `Good news! Looks like it will be a clear sky at midnight tonight - perfect for seeing the stars.`
+  } else if (midnightCloudCover[1] < 50) {
+    return `There's a ${midnightCloudCover[0]}% chance of clouds tonight. It looks like it will be a clearer sky tomorrow night.`
+  } else if (midnightCloudCover[2] < 50) {
+    return `There's a ${midnightCloudCover[0]}% chance of clouds tonight. It looks like it will be a clearer sky in 2 nights.`
+  } else if (midnightCloudCover[3] < 50) {
+    return `There's a ${midnightCloudCover[0]}% chance of clouds tonight. It looks like it will be a clearer sky in 3 nights.`
+  } else if (midnightCloudCover[4] < 50) {
+    return `There's a ${midnightCloudCover[0]}% chance of clouds tonight. It looks like it will be a clearer sky in 4 nights.`
+  } else {
+    return `Looks like it wll be cloudy for the next 5 nights. Netflix?`
+  }
+}
+
+export async function getWeatherData(lat, lng) {
+  const resp = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&APPID=ee4066bbc82305811aecd61e6c30d861`)
+  const json = await resp.json()
+  const midnightCloudCover = getMidnightCloudCover(json)
+  const weatherMessage = getWeatherMessage(midnightCloudCover)
+  return { midnightCloudCover, weatherMessage }
+}
